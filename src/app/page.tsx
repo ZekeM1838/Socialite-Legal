42fef969c193cc9d2f81f1e97ce375c7cb0fb4c4
ZekeM1838 <92056636+ZekeM1838@users.noreply.github.com>
2025-12-27 23:18:31 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import type { Metadata } from "next";
 
 // Force static generation
 export const dynamic = "force-static";
